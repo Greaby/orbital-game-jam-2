96b5413326d5d34fbf40e140be40d84fe998c4dc
Greaby <contact@greaby.com>
2019-03-02 22:28:48 +0100

    add levels

--- Main.gd
+++ Main.gd
@@ -1,10 +1,17 @@
 extends Node2D
 
+var tiles = [
+    "res://levels/Tile1.tscn",
+    "res://levels/Tile2.tscn",
+    "res://levels/Tile3.tscn",
+    "res://levels/Tile4.tscn"
+]
+
 func _ready():
     $Tiles/Tile.connect("new_tile", self, "add_tile")
 
 func add_tile(position):
-    var tile = load("res://levels/Tile.tscn").instance()
+    var tile = load(tiles[randi() % tiles.size()]).instance()
     tile.position = position
     tile.position.y -= tile.find_node("SpawnPosition").position.y
     tile.connect("new_tile", self, "add_tile")
